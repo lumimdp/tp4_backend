c4d54403ea9a58c6d517e6e1415b4c2f3f1d8c2e
lumimdp <73084324+lumimdp@users.noreply.github.com>
2023-10-07 21:31:41 -0300

    Add files via upload

---
+++ tp3_backend.php
@@ -0,0 +1,33 @@
+<?php
+
+for($a=1;$a<101;$a++)
+{
+ print "<p>$a</p>";	
+}
+
+for($d=100;$d>0;$d--)
+{
+ print "<p>$d</p>";	
+}
+
+for($p=2; $p<=100; $p= $p+2){
+ 
+    print "<p>$p</p>";
+} 
+
+for($i=1; $i<=100; $i= $i+2){
+ 
+    print "<p>$i</p>";
+}
+
+
+
+
+$base=0;
+
+for($i=1; $i<21; $i++){
+	$base+=$i;
+	print "$base</br>";
+}
+
+?>
